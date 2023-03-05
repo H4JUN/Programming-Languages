156b33aaf2ece16e31ad711517b7308e6bc6134f
tyson <ihazun01@gmail.com>
2023-03-04 22:44:10 -0500

    Updated prev command

--- C/shell/README.md
+++ C/shell/README.md
@@ -1,6 +1,7 @@
 # My implementation of the shell:
 - Supports input output redirection: e.g. "echo abcde > out" & "cat < README.md" 
 - Supports piping commands: e.g. "ls | wc | wc > out"
+- Supports "prev" command that allows the user to look up 10 most recent command-line inputs and prompts the user to enter the index of the cmd-line input to execute it.
 
 Usage:
 To build the binary:
@@ -10,4 +11,8 @@ make
 ```
 
 Example:
+- Piping and I/O redirection:
 ![image](example.png)
+
+- "prev" command:
+![image](example2.png)
--- C/shell/shell.c
+++ C/shell/shell.c
@@ -1,27 +1,45 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
-#include <errno.h>
 #include <stdlib.h>
-#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
 #include <wait.h>
 
 #define ever ;;
+#define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+#define MAX_COMMAND_CAPACITY 10
+#define BUFSIZE 128
+
+/* Double linked list for history of commands */
+typedef struct stack stack;
+
+struct stack {
+    char* command;
+    stack* prev;
+    stack* next;
+};
 
 void change_dir(char **);
-void run_command(char *);
-void run_command_helper(char *);
-void parse_string(char *, char **, int *, char*);
-void trim(char *, char *);
-void close_pipes(int [][2], int);
+void run_command(char **);
+void parse_string(char *, char **, int *);
+void add_command(char *, stack**, stack**);
+void free_cmd_stack(stack *);
+void show_cmd_stack(stack *);
+char* get_cmd_stack(stack *);
 
-int main() {
-    
-    int BUFSIZE = 128;
-    int arglen, wstat, pid;
+int COMMAND_CAPACITY = 0;
+
+int
+main() {
+
+    int i, arglen, wstat, pid;
     char *args[32];
-    char buf[BUFSIZE], copied_buf[BUFSIZE];
+    char buf[BUFSIZE];
     char *cwd, *user, *tok, *last_dir;
+    char *commandargs[32];
+    stack *cmd_stack = NULL;
+    stack *cmd_stack_tail = NULL;
 
     for(ever) {
         //Get user
@@ -34,25 +52,44 @@ int main() {
             last_dir = tok;
         }
         // Console:
-        printf("[%s@%s]~> ", user, last_dir);
+        printf(ANSI_COLOR_CYAN "[%s@%s]~> " ANSI_COLOR_RESET, user, last_dir);
         free(cwd);
 
         fgets(buf, BUFSIZE, stdin);
         if(buf[strlen(buf)-1] == '\n') {
             buf[strlen(buf)-1] = '\0';
         }
+PARSE:
+        add_command(buf, &cmd_stack, &cmd_stack_tail);
+
         // Arguments
-        // We need to pass a copy of the buf, so that we could keep the original one unmodified
-        strcpy(copied_buf, buf);
-        parse_string(copied_buf, args, &arglen, " \t\n");
+        parse_string(buf, args, &arglen);
+
+        // Prev
+        if(strcmp(args[0], "prev") == 0) {
+            show_cmd_stack(cmd_stack);
+            char* temp;
+            temp = get_cmd_stack(cmd_stack);
+            if(temp == NULL) {
+                fprintf(stderr, "Invalid command number\n");
+                continue;
+            }
+            else {
+                strncpy(buf, temp, strlen(temp));
+                buf[strlen(temp)] = '\0'; // Make sure buf does not read above temp's length
+                goto PARSE;
+            }
+        }
         // exit command
-        if(strcmp(args[0], "exit") == 0) {
+        else if(strcmp(args[0], "exit") == 0) {
+            free_cmd_stack(cmd_stack);
             exit(0);
         }
         // cd command
         else if(strcmp(args[0], "cd") == 0) {
             if(arglen > 2) {
                 fprintf(stderr, "%s: Too many arguments\n", args[0]);
+                continue;
             }
             else {
                 change_dir(args);
@@ -61,7 +98,7 @@ int main() {
         else {
             pid = fork();
             if(pid == 0) {
-                run_command(buf);
+                run_command(args);
             }
             else {
                 wait(&wstat);
@@ -70,14 +107,104 @@ int main() {
     }
 }
 
-void 
-change_dir(char **args) {
+char*
+get_cmd_stack(stack* head) {
     
+    char buf[BUFSIZE];
+    int target, counter = 1;
+    stack *trav = head;
+
+    printf("Enter command number: ");
+    fgets(buf, BUFSIZE, stdin);
+    if(buf[strlen(buf)-1] == '\n') {
+        buf[strlen(buf)-1] = '\0';
+    }
+
+    target = atoi(buf);
+    if(target > 10 || target < 1) {
+        return NULL;
+    }
+
+    while(trav) {
+        if(counter == target) {
+            return trav->command;
+        }
+        counter++;
+        trav = trav->next;
+    }
+    return NULL;
+}
+
+void
+show_cmd_stack(stack* head) {
+    int counter = 1;
+    stack* trav = head;
+
+    printf("-------------------------\nPrevious commands are:\n");
+    while(trav) {
+        printf("\t%d : %s\n", counter++, trav->command);
+        trav = trav->next;
+    }
+    printf("-------------------------\n");
+
+}
+
+void
+free_cmd_stack(stack* head) {
+    stack *prev = NULL;
+    while (head) {
+        if (prev) {
+            free(prev);
+        }
+        prev = head;
+        head = head->next;
+    }
+    free(prev);
+}
+
+void
+add_command(char *to_add, stack** head, stack** tail) {
+
+    if(strcmp(to_add, "prev") == 0) { // Skip "prev" command
+        return;
+    };
+    stack* prev = NULL;
+    stack* temp_stack = (stack *) malloc(sizeof(stack));
+    char* temp_cmd = (char *) malloc(strlen(to_add));
+    strcpy(temp_cmd, to_add);
+    temp_stack->command = temp_cmd;
+    if (*head == NULL) {
+        *head = *tail = temp_stack;
+        (*head)->next = NULL;
+        (*head)->prev = NULL;
+        COMMAND_CAPACITY++;
+        return;
+    }
+    else {
+        temp_stack->next = *head;
+        (*head)->prev = temp_stack;
+        (*head) = (*head)->prev;
+        (*head) = temp_stack;
+        COMMAND_CAPACITY++;
+    }
+    if (COMMAND_CAPACITY > MAX_COMMAND_CAPACITY) {
+        prev = (*tail)->prev;
+        prev->next = NULL;
+        free((*tail)->command);
+        free(*tail);
+        *tail = prev;
+        COMMAND_CAPACITY--;
+    }
+}
+
+void
+change_dir(char **args) {
+
     char *user, *home; 
     int n;
-    
+
     user = getlogin();
-    
+
     if(args[1] == NULL) { // Switch to home directory if used without given path
         // We won't get it from the environment. We'll just assume that it's /home/$USER
         home = malloc(7+strlen(user)); // '/home/' = 6, '\0' = 1 => 7
@@ -103,193 +230,30 @@ change_dir(char **args) {
 }
 
 void
-run_command(char *cmd) {
+run_command(char **args) {
 
-    char *pipe_ptr;
-    char *subcmd[32]; // To store the parsed commands by pipes
-    char trimmed[32]; // To store the trimmed command
-    int i, pid, subcmd_len, numPipes, pipe_flag;
+    int n;
 
-    pipe_flag = 0;
-
-    //First, check the presence of pipe:
-    pipe_ptr = strchr(cmd, '|');
-    if(pipe_ptr != NULL) { // '|' exists:
-        pipe_flag = 1;
-        parse_string(cmd, subcmd, &subcmd_len, "|");
-    }
-    if(pipe_flag == 1) {
-        numPipes = subcmd_len - 1; // Number of pipes is number of parsed commands - 1
-        int pfd[numPipes][2]; // Declare the number of pipes we need
-        for(i = 0; i < numPipes; i++) {
-            pipe(pfd[i]);   // Initialize the pipes
-        }
-        // Handle each command in subcmd
-        for(i = 0; subcmd[i] != NULL; i++) { // First command
-            pid = fork();
-            if(pid == 0) {
-                if(i == 0) {
-                    close(1);
-                    dup(pfd[i][1]);
-                    close_pipes(pfd, numPipes);
-                    trim(subcmd[i], trimmed);
-                    run_command_helper(trimmed);
-                }
-                else if(subcmd[i+1] == NULL) { // Last command
-                    close(0);
-                    dup(pfd[i-1][0]);
-                    close_pipes(pfd, numPipes);
-                    trim(subcmd[i], trimmed);
-                    run_command_helper(trimmed);
-                }
-                else { // Middle Command
-                    close(0);
-                    dup(pfd[i-1][0]);
-                    close(1);
-                    dup(pfd[i][1]);
-                    close_pipes(pfd, numPipes);
-                    trim(subcmd[i], trimmed);
-                    run_command_helper(trimmed);
-                }
-            }
-        }
-        close_pipes(pfd, numPipes);
-        // Wait for children
-        for(i = 0; i < subcmd_len; i++) {
-            wait(NULL);
-        }
-        exit(0);
-    }
-    else { // No pipe was found
-        run_command_helper(cmd);
-    }
-}
-
-
-void
-run_command_helper(char *cmd) {
-   
-    int output_arglen, input_arglen, cmd_arglen;
-    int append_redir_flag, overwrite_redir_flag, input_redir_flag;
-    char *double_redir, *one_redir, *inp_redir;
-    char output_redir_file[32], input_redir_file[32];
-    char *output_args[32], *input_args[32], *cmdargs[32];
-    int fd, n;
-
-    // Initialize flags:
-    append_redir_flag = 0;
-    overwrite_redir_flag = 0;
-    input_redir_flag = 0;
-    // First, take care of output redirections
-    //Check if output redir exists:
-
-    double_redir = strstr(cmd, ">>"); // ">>" redirection
-    one_redir = strchr(cmd, '>');   //  '>' redirection
-    if(double_redir != NULL) { // Case when ">>" exists
-        append_redir_flag = 1;
-        parse_string(cmd, output_args, &output_arglen, ">>");
-        // The last element is the file to append to. Remove any whitespace.
-        trim(output_args[output_arglen-1], output_redir_file);
-    }
-    else if(one_redir != NULL) {
-        overwrite_redir_flag = 1;
-        parse_string(cmd, output_args, &output_arglen, ">");
-        // The last element is the file to overwrite to. Remove any whitespace.
-        trim(output_args[output_arglen-1], output_redir_file);
-    }
-    else { // if both are NULL, then no output redirection
-        output_args[0] = cmd;
-    }
-    inp_redir = strchr(output_args[0], '<'); // Look for '<' in the command
-    if(inp_redir != NULL) {
-        input_redir_flag = 1;
-        parse_string(output_args[0], input_args, &input_arglen, "<");
-        // The last element is the file to redirect input from. Remove any whitespace.
-        trim(input_args[input_arglen-1], input_redir_file);
-    }
-    else {
-        input_args[0] = output_args[0];
-    }
-    // Now parse the actual command without the '<', '>', ">>":
-    parse_string(input_args[0], cmdargs, &cmd_arglen, " \t\n");
-    
-    if(append_redir_flag) {
-        fd = open(output_redir_file, O_WRONLY|O_APPEND);
-        if(fd == -1) {
-            perror("open");
-            exit(1);
-        }
-        close(1);
-        dup(fd);
-        close(fd);
-    }
-    else if(overwrite_redir_flag) {
-        fd = open(output_redir_file, O_CREAT|O_WRONLY|O_TRUNC, 0644);
-        if(fd == -1) {
-            perror("open");
-            exit(1);
-        }
-        close(1);
-        dup(fd);
-        close(fd);
-    }
-    if(input_redir_flag) {
-        fd = open(input_redir_file, O_RDONLY);
-        if(fd == -1) {
-            perror("open");
-            exit(1);
-        }
-        close(0);
-        dup(fd);
-        close(fd);
-    }
-    n = execvp(cmdargs[0], cmdargs);
+    n = execvp(args[0], args);
     if(n == -1) {
-        printf("Error is exec\n");
-        perror(cmdargs[0]);
+        perror(args[0]);
         exit(2);
-    } 
+    }
 }
 
 void
-parse_string(char *to_parse, char **parsed, int *parsed_len, char *delim) {
-    
+parse_string(char *to_parse, char **parsed, int *parsed_len) {
+
     int i;
-    
+
     *parsed_len = 0;
-    parsed[0] = strtok(to_parse, delim);
+    parsed[0] = strtok(to_parse, " \t\n");
     (*parsed_len)++;
     for(i = 1; i < 32; i++) {
-        parsed[i] = strtok(NULL, delim);
+        parsed[i] = strtok(NULL, " \t\n");
         if(parsed[i] == NULL) {
             break;
         }
         (*parsed_len)++;
-    }
-}
-
-void
-trim(char *to_trim, char *post_trim) {
-    
-    while(*to_trim != '\0') {
-        if(*to_trim != ' ') {
-            *post_trim = *to_trim;
-            post_trim++;
-            to_trim++;
-        }
-        else {
-            to_trim++;
-        }
-    }
-    *post_trim = '\0';
-}
-
-void
-close_pipes(int pfd[][2], int numPipes) {
-    
-    int j;
-    for(j = 0; j < numPipes; j++) {
-        close(pfd[j][0]);
-        close(pfd[j][1]);
     }
 }
